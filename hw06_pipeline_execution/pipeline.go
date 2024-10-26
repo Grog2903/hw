3a2c18d8289892280ce52bd8be876b3f27ba7119
Кухтин Григорий Сергеевич <kukhtings@pik.ru>
2024-10-26 21:46:08 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	current := in
+
+	for _, stage := range stages {
+		current = runStage(current, done, stage)
+	}
+
+	return current
+}
+
+func runStage(in In, done In, stage Stage) Out {
+	out := make(chan interface{})
+
+	go func() {
+		defer close(out)
+
+		stageOut := stage(in)
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-stageOut:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
