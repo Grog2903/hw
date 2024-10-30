f2a72f990f083cd28196fb71f8e774c9be2a9ed2
Кухтин Григорий Сергеевич <kukhtings@pik.ru>
2024-10-30 20:59:49 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +14,52 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0400)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer sourceFile.Close()
+
+	stat, err := sourceFile.Stat()
+	if err != nil {
+		return errors.New("get source file stat")
+	}
+	fileSize := stat.Size()
+
+	destinationFile, err := os.Create(toPath)
+	if err != nil {
+		return errors.New(fmt.Sprintf("create destination file to path: %s", toPath))
+	}
+	defer destinationFile.Close()
+
+	if offset > stat.Size() {
+		os.Remove(toPath)
+		return ErrOffsetExceedsFileSize
+	}
+
+	if offset > 0 {
+		_, err := sourceFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			os.Remove(toPath)
+			return errors.New("seek source file offset")
+		}
+	}
+
+	bytesToCopy := fileSize - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	progressBar := pb.Simple.Start64(bytesToCopy)
+	progressBarReader := progressBar.NewProxyReader(io.LimitReader(sourceFile, bytesToCopy))
+
+	_, err = io.CopyN(destinationFile, progressBarReader, bytesToCopy)
+	if err != nil {
+		os.Remove(toPath)
+		return errors.New("copy file")
+	}
+
+	progressBar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/require"
+	"os"
+	"os/exec"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("bashTests", func(t *testing.T) {
+		cmd := exec.Command("bash", "test.sh")
+
+		output, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Logf("Output:\n%s", string(output))
+		}
+
+		require.NoError(t, err)
+	})
+
+	t.Run("unsupportedFile", func(t *testing.T) {
+		err := Copy("testdata/input1.txt", "out.txt", 0, 0)
+
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error %q", err)
+	})
+
+	t.Run("bigOffset", func(t *testing.T) {
+		file, _ := os.Open("testdata/input.txt")
+		fileInfo, _ := file.Stat()
+		fileSize := fileInfo.Size()
+		offset := fileSize + 1
+
+		err := Copy("testdata/input.txt", "out.txt", offset, 0)
+
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error %q", err)
+	})
 }
