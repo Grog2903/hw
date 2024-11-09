1c375f88e4c12bc2afc570bd78df3f1e5f49b30f
Кухтин Григорий Сергеевич <kukhtings@pik.ru>
2024-11-09 17:16:08 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,58 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("emptyCommand", func(t *testing.T) {
+		env := Environment{}
+		returnCode := RunCmd([]string{}, env)
+
+		require.Equal(t, returnCode, 1)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		env := Environment{
+			"TEST_VAR": {Value: "123", NeedRemove: false},
+		}
+		cmd := []string{"sh", "-c", "echo $TEST_VAR"}
+		returnCode := RunCmd(cmd, env)
+
+		require.Equal(t, returnCode, 0)
+	})
+
+	t.Run("removeEnv", func(t *testing.T) {
+		err := os.Setenv("REMOVE_VAR", "should_be_removed")
+		require.NoError(t, err)
+
+		value := os.Getenv("REMOVE_VAR")
+		require.Equal(t, "should_be_removed", value)
+
+		env := Environment{
+			"REMOVE_VAR": {Value: "", NeedRemove: true},
+		}
+		cmd := []string{"sh", "-c", "echo $REMOVE_VAR"}
+
+		returnCode := RunCmd(cmd, env)
+		require.Equal(t, 0, returnCode)
+
+		value = os.Getenv("REMOVE_VAR")
+		require.Equal(t, "", value)
+	})
+
+	t.Run("bashTests", func(t *testing.T) {
+		cmd := exec.Command("bash", "test.sh")
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Logf("Output:\n%s", string(output))
+		}
+
+		require.NoError(t, err)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	envDir, shell, scriptPath string
+	scriptArgs                []string
+)
+
+func init() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go-envdir <envdir_path> <shell> <script_path> [script_args...]")
+		os.Exit(2)
+	}
+
+	envDir = os.Args[1]
+	shell = os.Args[2]
+	scriptPath = os.Args[3]
+	scriptArgs = os.Args[4:]
+}
+
 func main() {
-	// Place your code here.
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	cmd := append([]string{shell, scriptPath}, scriptArgs...)
+	exitCode := RunCmd(cmd, env)
+	os.Exit(exitCode)
 }
